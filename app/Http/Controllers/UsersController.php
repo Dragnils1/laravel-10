98df42640ca874225d28869a3a9030b9f8b2c380
dragnils1 <m.sharabarin05@mail.ru>
2023-04-18 22:45:21 +0600

    Pagination with bootstrap

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -13,7 +13,7 @@ class UsersController extends Controller
      */
     public function index()
     {
-        $users = User::get();
+        $users = User::paginate(10);
         return view('index', compact('users'));
         //
     }
